def41f66d20e57fff1fecb98fd5b912ea3235e50
William Kentris <themanfromjapan15@gmail.com>
2025-03-01 18:40:30 -0500

    Starting fresh

--- src/Pages/AnimeBingo.js
+++
@@ -1,5 +0,0 @@
-const AnimeBingo = () => {
-    return <h1>AnimeBingo</h1>;
-  };
-  
-  export default AnimeBingo;
--- src/Pages/WheelSpin.js
+++
@@ -1,5 +0,0 @@
-const WheelSpin = () => {
-    return <h1>WheelSpin</h1>;
-  };
-  
-  export default WheelSpin;
